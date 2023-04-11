f60bc3ed90f44855aefef113b8166db417602bc7
Ekaterina L <Ekaterin.Lys@yandex.ru>
2023-04-11 03:35:04 +0300

    corrected popup/task.js

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,15 +1,20 @@
 const popup = document.getElementById("subscribe-modal");
 const closeBtn = document.querySelector(".modal__close");
 
-if (!document.cookie) {
+if ( !findCookie("closedPopup") ) {
     popup.classList.add("modal_active");
-};
+}
 
 closeBtn.addEventListener("click", () => {
     popup.classList.remove("modal_active");
-    document.cookie = "closedPopup=true; expires=" + date;
-    // document.cookie = "closedPopup=true; max-age=3600";
+    
+    let date = new Date(Date.now() + 36e5);
+    date = date.toUTCString();
+    // document.cookie = "closedPopup=true; expires=" + date;
+    document.cookie = "closedPopup=true; max-age=600";
 });
 
-let date = new Date(Date.now() + 36e5);
-date = date.toUTCString();
+function findCookie(name) {
+    const pairs = document.cookie.split("; ");
+    return pairs.some( pair => pair.startsWith(name + "=") );
+}
