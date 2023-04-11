16b511cba020ee6bf15fc8fe3b1de9c657d5d4e7
Ekaterina L <Ekaterin.Lys@yandex.ru>
2023-04-11 04:19:22 +0300

    corrected text-editor: task.js, task.css

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,31 +1,82 @@
-const storedEditor = localStorage.getItem("editor");
+// const storedEditor = localStorage.getItem("editor");
 
-if (storedEditor) {
-    editor.value = storedEditor;
-};
+// if (storedEditor) {
+//     editor.value = storedEditor;
+// };
 
-document.getElementById("editor").addEventListener("blur", () => {
-    const editor = document.getElementById("editor");
-    localStorage.setItem("editor", editor.value);
-});
+// document.getElementById("editor").addEventListener("change", () => {
+// // document.getElementById("editor").addEventListener("blur", () => {
+//     const editor = document.getElementById("editor");
+//     localStorage.setItem("editor", editor.value);
+// });
 
-const btnClear = document.querySelector(".btn_clear");
+// const btnClear = document.querySelector(".btn_clear");
 
-btnClear.addEventListener("click", e => {
-    e.preventDefault();
-    editor.value = "";
-    // localStorage.setItem("editor", editor.value);
-    localStorage.removeItem("editor");
-})
+// btnClear.addEventListener("click", e => {
+//     e.preventDefault();
+//     editor.value = "";
+//     // localStorage.setItem("editor", editor.value);
+//     localStorage.removeItem("editor");
+// });
 
-// ------------------------------------------------------------------
+// ------------------------------------------------------------------ value включено в editor
 // const storedEditor = localStorage.getItem("editor");
 
 // if (storedEditor) {
 //     editor = storedEditor;
 // };
 
-// document.getElementById("editor").addEventListener("blur", () => {
+// document.getElementById("editor").addEventListener("change", () => {
 //     const editor = document.getElementById("editor").value;
 //     localStorage.setItem("editor", editor);
 // });
+
+// const btnClear = document.querySelector(".btn_clear");
+
+// btnClear.addEventListener("click", e => {
+//     e.preventDefault();
+//     editor.value = "";
+//     // localStorage.setItem("editor", editor.value);
+//     localStorage.removeItem("editor");
+// });
+
+// ------------------------------------------------------------------------------- c input
+const storedEditor = localStorage.getItem("editor");
+
+if (storedEditor) {
+    editor.value = storedEditor;
+};
+
+const btnClear = document.querySelector(".btn_clear");
+
+btnClear.addEventListener("click", e => {
+    e.preventDefault();
+    editor.value = "";
+    localStorage.removeItem("editor");
+});
+
+// -------------------------------------------------------------------------------
+document.getElementById("editor").addEventListener("input", e => {
+    e.preventDefault();
+    setTimeout( () => {
+        const editor = document.getElementById("editor");
+        localStorage.setItem("editor", editor.value);
+    }, 1500 );
+});
+
+// -------------------------------------------------------------------------------
+// const saveEditor = () => {
+//     document.getElementById("editor").addEventListener("change", () => {
+//         const editor = document.getElementById("editor");
+//         localStorage.setItem("editor", editor.value);
+//     });
+// };
+
+// function debounceDecorator(func) {
+//     return function(...args) {
+//         setTimeout( () => func(), 800 );
+//     }
+// }
+
+// const debouncedSaveEditor = debounceDecorator(saveEditor);
+// debouncedSaveEditor();
